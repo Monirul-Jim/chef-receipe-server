e7b61f6c4c27180328b49124a2ca837d6a33bcfc
unknown <Monirul-Jim>
2023-05-02 00:40:05 +0600

    load data

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express=require('express')
 const app=express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const cors=require('cors')
+
+app.use(cors())
+
+const data=require('./data/data.json')
 
 app.get('/',(req,res)=>{
     res.send('the chef recipe ')
+})
+app.get('/data',(req,res)=>{
+    res.send(data)
 })
 app.listen(port,(req,res)=>{
     console.log('the chef recipe is loading');
